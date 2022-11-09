a42e4d35288680c70781c42b37a8556c17c0b820
Jorge Arthur Schneider Aranda <jorgearthurs@hotmail.com>
2022-11-08 22:51:24 -0300

    contacts mock

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Contact } from './Contact.jsx';
+
+const contacts = [
+
+  {_id:1, name:'Fulano da silva', whatsapp:'999587412', email:'something@gmail.com', subject:'loan for a new house'},
+  {_id:2, name:'Fulana da silva', whatsapp:'999587413', email:'something123@gmail.com', subject:'loan for a new house'},
+  {_id:3, name:'ciclano da silva', whatsapp:'999545412', email:'something456@gmail.com', subject:'rent a house'},
+];
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    
+    <ul>
+      {contacts.map(contact => <Contact key={ contact._id } contact={ contact }/>)}
+    </ul>
+
   </div>
 );
